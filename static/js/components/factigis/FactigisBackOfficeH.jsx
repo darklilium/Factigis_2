b84c6e20b5b7db4b778bf32a07128f1c8b0de4fe
Evelyn Hernández <dark.desires@live.cl>
2017-01-06 10:31:03 -0300

    Factigis v0.4.2 prod

--- static/js/components/factigis/FactigisBackOfficeH.jsx
+++ static/js/components/factigis/FactigisBackOfficeH.jsx
@@ -338,7 +338,7 @@ class FactigisBackOfficeH extends React.Component {
       }, "BMToggle2");
       toggle.startup();
 
-      const page = "REACT_FACTIGIS_DESA";
+      const page = "REACT_FACTIGIS_PROD";
       const module = "FACTIGIS_REVISAR_HISTORIAL_FACTIBILIDAD";
       const date = getFormatedDate();
       const user = cookieHandler.get('usrprfl')
